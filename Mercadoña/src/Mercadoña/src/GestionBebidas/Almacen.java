61fe98341c0309133fb0d5d8d54fcdc2df7903ec
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2021-01-26 14:23:57 +0100

    2

---
+++ Mercadoña/src/Mercadoña/src/GestionBebidas/Almacen.java
@@ -0,0 +1,5 @@
+package GestionBebidas;
+
+public class Almacen {
+
+}
---
+++ Mercadoña/src/Mercadoña/src/Mercadoña/Perecedero.java
@@ -0,0 +1,46 @@
+package Mercadoña;
+
+public final class Perecedero extends Producto {
+
+	private int dias;
+
+	public Perecedero(String nombre, float precio, int dias) {
+		super(nombre, precio);
+		this.dias = dias;
+	}
+
+	public int getDias() {
+		return dias;
+	}
+
+	public void setDias(int dias) {
+		this.dias = dias;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Perecedero [dias=" + dias + "]";
+	}
+
+	@Override
+	public float calcular(int cantidad) {
+	//	float precioFinal = super.calcular(cantidad);
+		float precioFinal = cantidad * precio;
+		switch (dias) {
+		case 1:
+			precioFinal /= 4;
+			break;
+
+		case 2:
+			precioFinal = precioFinal/3;
+			break;
+
+		case 3:
+			precioFinal = precioFinal/2;
+			break;
+
+		}
+		
+		return precioFinal;
+	}
+}
