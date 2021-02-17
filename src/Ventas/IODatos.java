dcd246d60631b9326895b778ba7192911121df6a
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2021-02-17 14:26:30 +0100

    Ventas actualizado

--- src/Ventas/IODatos.java
+++ src/Ventas/IODatos.java
@@ -1,7 +1,9 @@
 package Ventas;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -38,4 +40,34 @@ public class IODatos {
 			}
 	}
 }
+	public static Ventas[] cargarDatos(String rutaFichero) {
+		File f = new File(rutaFichero);
+		Ventas[] vVentas = new Ventas[10];
+		String cliente="";
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try (FileInputStream fi = new FileInputStream(f);
+					DataInputStream leer = new DataInputStream(fi)){
+					
+				while(true) {
+					cliente = leer.readUTF();
+					}
+			}catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				
+				System.out.println("Fin de lectura de fichero");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Fin de lectura de fichero");
+			}
+				return Ventas[];
+			
+		}
+	}
 }
