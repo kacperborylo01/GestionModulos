22c29c580d84c767e99045a059fcd1b0c8db37f5
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2021-02-19 14:33:06 +0100

    1

--- src/Ventas/IODatos.java
+++ src/Ventas/IODatos.java
@@ -7,11 +7,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Iterator;
 
 public class IODatos {
 
-	public static void guardarDatos(String rutaFichero, Ventas[] datos ) {
+	public static void guardarDatos(String rutaFichero, Ventas[] vVentas ) {
 		File f = new File(rutaFichero);
 		if (!f.exists()) {
 			try {
@@ -23,7 +25,7 @@ public class IODatos {
 			
 			try (FileOutputStream fo = new FileOutputStream(f);
 					DataOutputStream escribir = new DataOutputStream(fo)){
-				for (Ventas ventas : datos) {
+				for (Ventas ventas : vVentas) {
 					if (ventas != null) {
 						escribir.writeUTF(ventas.getCliente());
 						escribir.writeInt(ventas.getProducto());
@@ -40,10 +42,41 @@ public class IODatos {
 			}
 	}
 }
+	public static void guardarObjetos(String rutaFichero, Ventas[] vVentas ) {
+		File f = new File(rutaFichero);
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try (FileOutputStream fo = new FileOutputStream(f);
+					ObjectOutputStream escribir = new ObjectOutputStream(fo)){
+				for (Ventas ventas : vVentas) {
+					if (ventas != null) {
+						escribir.writeObject(ventas);;
+						
+					}
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	}
+}
 	public static Ventas[] cargarDatos(String rutaFichero) {
 		File f = new File(rutaFichero);
 		Ventas[] vVentas = new Ventas[10];
 		String cliente="";
+		int producto;
+		int cantidad;
+		double precioUnitario;
+		int cont=0;
 		if (!f.exists()) {
 			try {
 				f.createNewFile();
@@ -57,7 +90,15 @@ public class IODatos {
 					
 				while(true) {
 					cliente = leer.readUTF();
-					}
+					producto = leer.readInt();
+					cantidad = leer.readInt();
+					precioUnitario = leer.readDouble();
+					
+					Ventas v = new Ventas(cliente, producto, cantidad, precioUnitario);
+					vVentas[cont] = v;
+					cont++;
+				
+				}
 			}catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				
@@ -66,8 +107,55 @@ public class IODatos {
 				// TODO Auto-generated catch block
 				System.out.println("Fin de lectura de fichero");
 			}
-				return Ventas[];
+				
 			
 		}
+		return vVentas;
+	}
+	public static Ventas[] cargarObjetos(String rutaFichero) {
+		File f = new File(rutaFichero);
+		Ventas[] vVentas = new Ventas[10];
+		String cliente="";
+		int producto;
+		int cantidad;
+		double precioUnitario;
+		int cont=0;
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try (FileInputStream fi = new FileInputStream(f);
+					ObjectInputStream leer = new ObjectInputStream(fi)){
+					
+				while(true) {
+					
+					
+					Ventas v;
+					try {
+						v = (Ventas)leer.readObject();
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					vVentas[cont] = v;
+					cont++;
+				
+				}
+			}catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				
+				System.out.println("Fin de lectura de fichero");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Fin de lectura de fichero");
+			}
+				
+			
+		}
+		return vVentas;
 	}
 }
--- src/Ventas/Main.java
+++ src/Ventas/Main.java
@@ -1,17 +1,76 @@
 package Ventas;
 
+import Ventas.IODatos;
+import Ventas.Ventas;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*Ventas[] vVentas = new Ventas[10];
+		
+		vVentas[0] = new Ventas("Juan", 1, 1, 10);
+		vVentas[0] = new Ventas("Pepe", 1, 2, 20);
+		vVentas[0] = new Ventas("Ivan", 2, 1, 10);
+		vVentas[0] = new Ventas("Leo", 1, 1, 10);
+		vVentas[0] = new Ventas("Isa", 1, 1, 10);
+		
+		IODatos.guardarDatos("ventas.dat", vVentas);*/
+	Ventas[] vVentas = IODatos.cargarDatos("ventas.dat");
+		
+		
 
-		Ventas vVentas[] = new Ventas[10];
+        for (Ventas ventas : vVentas) {
+            if ((ventas != null)) {
+                System.out.println(ventas);
+            }
+        }
+			
+		}
+private static double calcularTotalCliente(Ventas[] vVentas, String cliente) {
 		
-		vVentas[0] = new Ventas("Ignacio", 123, 2, 1.12);
-		vVentas[1] = new Ventas("Juan Carlos", 1234, 3, 3.12);
-		vVentas[2] = new Ventas ("Mario",12345,3,4.56);
+		int suma = 0,total=0;
 		
-		 IODatos.guardarDatos("ventas.dat", vVentas);
+		for (Ventas ventas : vVentas) {
+			if (ventas!=null && ventas.getCliente().equalsIgnoreCase(cliente)){
+				total+= ventas.getPrecioUnitario("El total de ventas de Juan es " + ventas);
+			}
+		}
+		
+		return suma;
+	}
+	
+	private static int contarClientes(Ventas[] vVentas, String cliente) {
+		
+		int total = 0;
+		String[] vNombres = new String[vVentas.length];
+		
+		
+		for (Ventas ventas : vVentas) {
+			if (ventas!=null) {
+				for (int i = 0; i<vNombres.length;i++) {
+					if (vNombres[i]!=null) {
+						if (ventas.getCliente().equalsIgnoreCase(vNombres[i])) {
+							
+							break;
+						}
+					}else {
+						 
+							total++;
+							vNombres[i] = ventas.getCliente();
+						
+						break;
+					}
+				}
+			}else {
+				break;
+			}
+		}
+		
+		
+		
+		return total;
+	}
 	}
 
-}
+
